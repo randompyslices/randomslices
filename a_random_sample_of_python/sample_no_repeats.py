5c49a4c961ad1ff46f177d9c37974e35c7b3c9e3
Mike <webmaster@randomslices.com>
2014-04-14 23:46:43 -0700

    Some timing examples

---
+++ a_random_sample_of_python/sample_no_repeats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from random import randint
+import numpy as np
+from timeit import timeit
+
+def sample_without_repeats(M,N):
+    while True:
+        s = [randint(0,M) for i in xrange(0,2*N)][:N]
+        if len(set(s)) == N:
+            return s
+
+def sample_no_repeats(M,N):
+    while True:
+        s = np.random.randint(0,M,N)
+        if len(set(s)) == N:
+            return s
+
+print 'NumPy: ', timeit('sample_no_repeats(1000,100)', 
+                setup='from __main__ import sample_no_repeats',number=100)
+
+print 'randint: ', timeit('sample_without_repeats(1000,100)', 
+                setup = 'from __main__ import sample_without_repeats',number=100)
+
+print 'random.sample: ', timeit('random.sample(xrange(0,1000),100)',setup='import random',number=100)
+
+
